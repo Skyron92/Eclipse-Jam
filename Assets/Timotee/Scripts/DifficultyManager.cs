caaed38bc6fa1eddfb747a8dfa789073f13f0e44
TheMoonBeard <timotee.baboulin@gmail.com>
2024-01-03 17:12:42 +0100

    Plein de trucs frr

--- Assets/Timotee/Scripts/DifficultyManager.cs
+++ Assets/Timotee/Scripts/DifficultyManager.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 public class DifficultyManager : MonoBehaviour{
     public static DifficultyManager Instance;
 
-    public static int MaxCharacters => Instance._characterCount;
+    public static int MaxCharacters{
+        get{
+            if (ThirdPhase)
+                return Instance._thirdPhaseCharacters;
+            if (SecondPhase)
+                return Instance._secondPhaseCharacters;
+            return Instance._characterCount;
+        }
+    }
     [SerializeField] private int _characterCount;
+    [SerializeField] private int _secondPhaseCharacters;
+    [SerializeField] private int _thirdPhaseCharacters;
 
     public static float GrowthSpeed => Instance._growthSpeed;
     [SerializeField] private float _growthSpeed;
 
+    public static bool SecondPhase => GameManager.Timer >= Instance._secondPhaseThreshold;
+    [SerializeField] private float _secondPhaseThreshold;
+
+    public static bool ThirdPhase => GameManager.Timer >= Instance._thirdPhaseThreshold;
+    [SerializeField] private float _thirdPhaseThreshold;
+    
     private void OnEnable(){
         if (Instance == null){
             Instance = this;
@@ -18,4 +34,6 @@ public class DifficultyManager : MonoBehaviour{
             Destroy(gameObject);
         }
     }
+    
+    
 }
