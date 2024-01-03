82ad22e7c0a465c0bbb7fb1ecd61f08a3f7dc5aa
Skyron <courtier.leo@gmail.com>
2024-01-03 01:08:56 +0100

    Add new input system (et j'ai posé ma merde aussi)

---
+++ Assets/Leo(Touchez pas c'est mes affaires)/Scripts/EarthRotation.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EarthRotation : MonoBehaviour
+{
+    [SerializeField] private InputActionReference rotateInputActionReference;
+}
